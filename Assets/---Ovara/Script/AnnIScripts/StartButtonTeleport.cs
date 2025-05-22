439b20bcf9dca237e0f14b4890672b170acdbda3
annikepo <anni.kepczynski@hyperisland.se>
2025-05-22 17:00:37 +0200

    Teleport from start button to spot1

---
+++ Assets/---Ovara/Script/AnnIScripts/StartButtonTeleport.cs
@@ -0,0 +1,23 @@
+﻿
+using UnityEngine;
+
+public class StartButtonTeleport : MonoBehaviour
+{
+    public Transform trackingSpace;   // <- Assign [BuildingBlock] Camera Rig → TrackingSpace
+    public Transform targetSpot;      // <- Assign Spot 1
+
+    public void MoveToSpotOne()
+    {
+        Debug.Log("Teleport triggered");
+
+        if (trackingSpace != null && targetSpot != null)
+        {
+            trackingSpace.position = targetSpot.position;
+            Debug.Log("Teleported to Spot 1");
+        }
+        else
+        {
+            Debug.LogWarning("Missing trackingSpace or targetSpot");
+        }
+    }
+}
